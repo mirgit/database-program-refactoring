473ddd418dbec89bd5ef3e9e4234c040c3b3b422
mirgit <mahi1390.76@gmail.com>
2021-07-09 00:59:22 +0430

    "using z3 for value correspondance"

--- schema.py
+++ schema.py
@@ -1,32 +1,18 @@
-# sum((len(v) for v in newSchema.values()))
-class schema:
+class Schema:
     def __init__(self, s):
         self.schema = s
-
         self.cols = {t + '.' + x: self.schema[t][x] for t in self.schema for x in self.schema[t]}
-        self.attr2id = {c: i for i, c in enumerate(self.cols)}
+        self.attr2id = {c: i+1 for i, c in enumerate(self.cols)}
         self.id2attr = {self.attr2id[c]: c for c in self.attr2id}
 
     def name_to_id(self, a):  # if not exist...!!!!!!!!
         return self.attr2id[a]
 
     def id_to_name(self, a):  # if not exist...!!!!!!!!
-        return (self.id2attr[a])
+        return self.id2attr[a]
 
     def id_to_type(self, a):  # if not exist...!!!!!!!!
-        return (self.cols[self.id2attr[a]])
+        return self.cols[self.id2attr[a]]
 
     def size(self):
         return len(self.attr2id)
-
-
-oldSchema = {'Class': {'ClassId': 'int', 'InstId': 'int', 'TaId': 'int'},
-             'Instructor': {'InstId': 'int', 'IName': 'string', 'IPic': 'picture'},
-             'TA': {'TaId': 'int', 'TName': 'string', 'TPic': 'picture'}}
-newSchema = {'Class': {'ClassId': 'int', 'InstId': 'int', 'TaId': 'int'},
-             'Instructor': {'InstId': 'int', 'IName': 'string', 'PicId': 'int'},
-             'TA': {'TaId': 'int', 'TName': 'string', 'PicId': 'int'}, 'Picture': {'PicId': 'int', 'Pic': 'picture'}}
-s2 = schema(newSchema)
-s1 = schema(oldSchema)
-s1.name_to_id('TA.TName')
-# s2.size()
--- value_correspondance.py
+++ value_correspondance.py
@@ -1,87 +1,85 @@
-# !pip install python-sat[pblib,aiger]
-# !pip install python-Levenshtein
-
-from pysat.formula import WCNF
 from Levenshtein import distance as levenshtein_distance
-# from pysat.solvers import Solver
-from pysat.examples.rc2 import RC2
 from schema import schema
+from z3 import *
 
-# !!!! bug: -0 is meaningless
-class val_corr_generator:
-    def __init__(self, S1, S2):
+
+class ValCorrGenerator:
+    def __init__(self, s1, s2):
         alpha = 15
-        size1 = S1.size()
-        size2 = S2.size()
+        size1 = s1.size()
+        size2 = s2.size()
         self.size1 = size1
-        self.S1 = S1
-        self.S2 = S2
+        self.S1 = s1
+        self.S2 = s2
 
-        self.cnf = RC2(WCNF())
+        self.cnf = Optimize()
+        x = [[]]
+        for i in range(1, size1+1):
+            x.append([None])
+            for j in range(1, size2+1):
+                x[i].append(Bool(str(i)+'_'+str(j)))
 
-        ## hard: variables !!!!!! really needed?
-        for i in range(size1 * size2):
-            self.cnf.add_clause([i, -i])
-
-        ## hard: type-matching-conditions
-        for i in range(size1):
-            for j in range(size2):
-                if S1.id_to_type(i) != S2.id_to_type(j):
-                    self.cnf.add_clause([-self.twoD2oneD(i, j)])
+        # hard: type-matching-constraints
+        for i in range(1, size1+1):
+            for j in range(1, size2+1):
+                if s1.id_to_type(i) != s2.id_to_type(j):
+                    self.cnf.add(Not(x[i][j]))
 
         ## hard: match each i to some j !!!!!!change to a_is that are queried in p
-        # js=[x for x in range(size2)]
-        for i in range(size1):
-            all_poss = [self.twoD2oneD(i, j) for j in range(size2)]
-            self.cnf.add_clause(all_poss)
+        for i in range(1, size1+1):
+            self.cnf.add(Or([x[i][j] for j in range(1, size2+1)]))
 
-        ## soft: prefer 1-1 to 1-many
-        for i in range(size1):
-            for j in range(size2):
-                for k in range(j, size2):
-                    self.cnf.add_clause([-self.twoD2oneD(i, j), -self.twoD2oneD(i, k)], weight=alpha)
+        # soft: prefer 1-1 to 1-many
+        for i in range(1, size1+1):
+            for j in range(1, size2+1):
+                for k in range(j, size2+1):
+                    self.cnf.add_soft(Or(Not(x[i][j]), Not(x[i][k])), weight=alpha)
 
-        ## soft: similar names: more score!!!!!! change similarity maybe..
-        for i in range(size1):
-            for j in range(size2):
-                similarity = 20 - levenshtein_distance(S1.id_to_name(i), S2.id_to_name(j))
-                self.cnf.add_clause([self.twoD2oneD(i, j)], similarity)
+        # soft: similar names: more score
+        for i in range(1, size1+1):
+            for j in range(1, size2+1):
+                similarity = 20 - levenshtein_distance(s1.id_to_name(i), s2.id_to_name(j))
+                self.cnf.add_soft(x[i][j], similarity)
+        self.vars = x
+        self.solution = {attr: [] for attr in self.S1.cols}  ##!!!!!!only queried attrs are needed
 
-        # last_sol?(use in update)
-        # self.solver = Solver()
-        # self.solver.append_formula(self.cnf.clauses, no_return=False)#!!!!!no_return?!
-
-    def max_sat_solver(self):
-        self.last_solution = self.cnf.compute()
-        # self.solver.solve()
-        # print(self.solver.get_model())
-        # solver.delete()
-
-    def max_sat_updater(self):
-        for x in self.last_solution:
-            self.cnf.add_clause([-x])
-
-    def return_sol(self):
-        matching = {}
-        for i in self.last_solution:
-            if i > 0:
-                a, b = self.oneD2twoD(i)
-                a = self.S1.id_to_name(a)
-                b = self.S2.id_to_name(b)
-                if a in matching:
-                    matching[a].append(b)
+    def get_solution(self):
+        check = self.cnf.check()
+        for k in self.solution.keys():
+            self.solution[k].clear()
+        if check == sat:
+            m = self.cnf.model()
+            solution = []
+            for var in m:
+                i, j = var.name().split('_')
+                if m[var]:
+                    solution.append(self.vars[int(i)][int(j)])
+                    self.solution[self.S1.id_to_name(int(i))].append(self.S2.id_to_name(int(j)))
                 else:
-                    matching[a] = [b]
-        print(matching)
+                    solution.append(Not(self.vars[int(i)][int(j)]))
+            self.cnf.push()
+            self.cnf.add(Not(And(solution)))
+        else:
+            return check
+        return self.solution
 
-    def twoD2oneD(self, i, j):
-        return j * self.size1 + i
-
-    def oneD2twoD(self, x):
-        return (x % self.size1, int(x / self.size1))
-
-# phi = val_corr_generator(s1, s2)
-# a = phi.max_sat_solver()
-# phi.return_sol()
+    def print_sol(self):
+        for k in self.solution:
+            print(k, '  -->  ', self.solution[k])
+        print('========================================================')
+# oldSchema = {'Class': {'ClassId': 'int', 'InstId': 'int', 'TaId': 'int'},
+#              'Instructor': {'InstId': 'int', 'IName': 'string', 'IPic': 'picture'},
+#              'TA': {'TaId': 'int', 'TName': 'string', 'TPic': 'picture'}}
+# newSchema = {'Class': {'ClassId': 'int', 'InstId': 'int', 'TaId': 'int'},
+#              'Instructor': {'InstId': 'int', 'IName': 'string', 'PicId': 'int'},
+#              'TA': {'TaId': 'int', 'TName': 'string', 'PicId': 'int'}, 'Picture': {'PicId': 'int', 'Pic': 'picture'}}
+# oldSchema = {'A': {'B': 'int', 'C': 'int'}}
+# newSchema = {'M': {'B': 'int'}, 'N': {'D': 'bool', 'E': 'int'}}
+# S1 = schema(oldSchema)
+# S2 = schema(newSchema)
 #
-# phi.max_sat_updater()
+# phi = ValCorrGenerator(S1, S2)
+# a = phi.get_solution()
+# while a != unsat:
+#     phi.print_sol()
+#     a = phi.get_solution()
