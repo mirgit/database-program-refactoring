1e19bcd38377f84978eab28c31a4c4cbfb7b399f
mirgit <mahi1390.76@gmail.com>
2021-07-13 12:16:27 +0430

    transactions corruption

--- sql_parser.py
+++ sql_parser.py
@@ -2,11 +2,12 @@ import sqlparse
 from Transactions import *
 
 
-q ='SELECT A.i, B.i FROM A JOIN B ON A.a = B.b WHERE A.c = 5'
-
-u = 'UPDATE A SET A.b = 5 WHERE A.c = 2'
-u = 'UPDATE customers SET customers.email = email WHERE customers.id = id;'
-u = 'INSERT INTO Customer (CustomerID, Fname) VALUES (id, name);'
+q = 'SELECT A.i, B.i FROM A JOIN B ON A.a = B.b WHERE A.c = 5'
+u = 'SELECT CustomerID FROM Customer WHERE Name = <name>;'
+u = 'SELECT order_items.order_id, items.name, order_items.quantity FROM order_items JOIN items ON order_items.item_id = items.id WHERE order_items.id = <id>;'
+# u = 'UPDATE A SET A.b = 5 WHERE A.c = 2'
+# u = 'UPDATE customers SET customers.email = email WHERE customers.id = id;'
+# u = 'INSERT INTO Customer (CustomerID, Fname) VALUES (id, name);'
 # u = 'DELETE FROM line_items WHERE line_items.id = <id>;'
 parsed_query = sqlparse.parse(u)[0]
 hq = sqlparse.parse(q)[0]
@@ -29,9 +30,9 @@ elif parsed_query[0].value == 'DELETE':
 
 elif parsed_query[0].value == 'INSERT':
     jc = parsed_query[4].value.split()[0].strip()
-    attrs = [i.replace(',','').strip() for i in parsed_query[4].value.replace('(','').replace(')','').split()[1:]]
-    vals = [i.replace(',','').strip() for i in parsed_query[6].value.replace('(','').replace(')','').split()[1:]]
+    attrs = [i.replace(',','').strip() for i in parsed_query[4].value.replace('(', '').replace(')', '').split()[1:]]
+    vals = [i.replace(',','').strip() for i in parsed_query[6].value.replace('(', '').replace(')', '').split()[1:]]
     ins = {attrs[i]:vals[i] for i in range(len(vals))}
-    transaction = Insert(jc,ins)
+    transaction = Insert(jc, ins)
 elif parsed_query[0].value == 'SELECT':
-    pass
+    attrs = [i.strip() for i in parsed_query[2].value.split(',')]
