8b5f960ee9a5419e64253eda86313e39fed1884b
f.labbaf98 <f.labbaf98@gmail.com>
2021-07-14 07:57:45 +0430

    sql and schema

---
+++ SqliteDB.py
@@ -0,0 +1,49 @@
+import sqlite3
+from sqlite3 import Error
+
+class SqliteDB:
+    def __init__(self, db_name):
+        self.db = self.create_connection(db_name)
+        # TODO create tables from schema
+
+
+    def create_connection(self, path):
+        connection = None
+        try:
+            connection = sqlite3.connect(path)
+            print("Connection to SQLite DB successful")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+        return connection
+
+
+    def execute_query(self, query):
+        cursor = self.db.cursor()
+        try:
+            cursor.execute(query)
+            self.db.commit()
+            print("Query executed successfully")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+            
+        cursor = self.db.cursor()
+        try:
+            cursor.execute(query)
+            self.db.commit()
+            print("Query executed successfully")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+
+    def execute_read_query(self,query):
+        cursor = self.db.cursor()
+        result = None
+        try:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result
+        except Error as e:
+            print(f"The error '{e}' occurred")
+            
+        
